b856f57f7fedb68efdbd646d9a9bf3fdf7a958bb
lyrasimchuk <asusenna@gmail.com>
2017-06-05 19:45:43 +0300

    Add files via upload

---
+++ Assets/DeathHere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DeathHere : MonoBehaviour {
+	//Стандартна функція, яка викличеться,
+	//коли поточний об’єкт зіштовхнеться із іншим
+	void OnTriggerEnter2D(Collider2D collider) {
+		//Намагаємося отримати компонент кролика
+		HeroRabit rab = collider.GetComponent<HeroRabit> ();
+		//Впасти міг не тільки кролик
+		if(rab != null) {
+			//Повідомляємо рівень, про смерть кролика
+			LevelController.current.onRabitDeath (rab);
+		}
+	}
+}
